51af494751e2adab4c4c045cfbea621c4171f112
Orisha <orisakwenwokocha1@gmail.com>
2024-01-20 11:29:15 +0100

    Add files via upload

---
+++ Medium/LongestSubstring.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+
+        char[] c = s.toCharArray();
+        ArrayList<Character> result = new ArrayList<>();
+        boolean condition = false;
+
+        int ind = 0;
+        for (int count = 0; count < c.length; count++) {
+            for (int index = 0; index < c.length; index++) {
+                if (c[index] == c[count] && count != index) {
+                    ind = index;
+                    condition = true;
+                    break;
+                }
+            }
+            if (condition) break;
+        }
+        System.out.println(ind);
+        if (ind == c.length - 1) {
+            for (char value : c) {
+                if (result.contains(value)) continue;
+                result.add(value);
+            }
+        }
+
+        else {
+            for (int count = ind; count < c.length; count++) {
+                if (result.contains(c[count])) continue;
+                result.add(c[count]);
+            }
+        }
+        System.out.println(result);
+        return result.size();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("cdd"));
+    }
+}
